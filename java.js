efcbcfc2282c386c3654114e28aa17d390cb8420
matulesy <114120155+matulesy@users.noreply.github.com>
2024-12-11 07:52:02 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+// Toggle hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
